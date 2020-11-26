c937d0f28bfc148753a999d6b3d3692e1c0e7f00
Gabriel Trindade <icandoitgrt@gmail.com>
2020-11-26 10:30:14 -0300

    Add código dps da orientação do Latorre

--- LG2A2 - 261049/Tarefa 02-01.java
+++ LG2A2 - 261049/Tarefa 02-01.java
@@ -1,60 +1,63 @@
 import java.util.Scanner;
 
-public class ex1 {
+public class Tarefa02-01 {
     public static void main(String[] args) {
 
         double num1, num2;
 
-       String opSoma = new String("soma");
-       String opSubtracao = new String("subtração");
-       String opMultiplicacao = new String("multiplicação");
-       String opDivisao = new String("divisão");
+        String opSoma = new String("soma");
+        String opSubtracao = new String("subtração");
+        String opMultiplicacao = new String("multiplicação");
+        String opDivisao = new String("divisão");
 
-       Scanner input = new Scanner(System.in);
-            System.out.println("Qual operação você deseja realizar? ");
-            System.out.println("Operações disponíveis: soma, subtração, multiplicação ou divisão");
-            System.out.print("Digite a operação: ");
-            String op = input.nextLine();
-            if (op.equalsIgnoreCase(opSoma) || op.equalsIgnoreCase(opSubtracao) || op.equalsIgnoreCase(opMultiplicacao) || op.equalsIgnoreCase(opDivisao) ) {
-                System.out.println("Entre com o primeiro número: ");
-                num1 = input.nextDouble();
-                System.out.println("Entre com o segundo número: ");
-                num2 = input.nextDouble();
-                if (op.equalsIgnoreCase(opSoma)) {
-                    add(num1, num2);
-                }else{
-                    if(op.equalsIgnoreCase(opSubtracao)){
-                        sub(num1, num2);
-                    }else{
-                        if(op.equalsIgnoreCase(opMultiplicacao)){
-                            multi(num1, num2);
-                        }else{
-                            if(op.equalsIgnoreCase(opDivisao)){
-                                div(num1, num2);
-                            }
-                        }
-                    }
-                }
-            }else {
-                System.out.println("Erro! Por favor, digite uma das operações disponíveis.");
+        Scanner input = new Scanner(System.in);
+        double result;
+        System.out.println("Qual operação você deseja realizar? ");
+        System.out.println("Operações disponíveis: soma, subtração, multiplicação ou divisão");
+        System.out.print("Digite a operação: ");
+        String op = input.nextLine();
+        if (op.equalsIgnoreCase(opSoma) || op.equalsIgnoreCase(opSubtracao) || op.equalsIgnoreCase(opMultiplicacao) || op.equalsIgnoreCase(opDivisao) ) {
+            System.out.println("Entre com o primeiro número: ");
+            num1 = input.nextDouble();
+            System.out.println("Entre com o segundo número: ");
+            num2 = input.nextDouble();
+            if (op.equalsIgnoreCase(opSoma)) {
+                result = add(num1, num2);
+                System.out.println("A soma entre os valores inseridos é " + result);
             }
+            if (op.equalsIgnoreCase(opSubtracao)) {
+                result = sub(num1, num2);
+                System.out.println("A subtração entre os valores inseridos é " + result);
+            }
+            if (op.equalsIgnoreCase(opMultiplicacao)) {
+                result = multi(num1, num2);
+                System.out.println("A multiplicação entre os valores inseridos é " + result);
+            }
+            if (op.equalsIgnoreCase(opDivisao)) {
+                if(num2 == 0)
+                    System.out.println("Erro! Divisor igual a 0.");
+                else {
+                    result = sub(num1, num2);
+                    System.out.println("A divisão entre os valores inseridos é " + result);
+                }
+            }
+
+        }else {
+            System.out.println("Erro! Por favor, digite uma das operações disponíveis.");
+        }
         input.close();
     }
 
-    public static void add(double n1, double n2){
-        System.out.println("A soma entre os valores inseridos é " + (n1 + n2));
+    public static double add(double n1, double n2){
+        return (n1+n2);
     }
-    public static void multi(double n1, double n2){
-        System.out.println("A multiplicação entre os valores inseridos é " + (n1 * n2));
+    public static double multi(double n1, double n2){
+        return (n1*n2);
     }
-    public static void sub(double n1, double n2){
-        System.out.println("A subtração entre os valores inseridos é " + (n1 - n2));
+    public static double sub(double n1, double n2){
+        return (n1-n2);
     }
-    public static void div(double n1, double n2){
-        if(n2 == 0){
-            System.out.println("Erro! Divisor igual a 0.");
-        }else {
-            System.out.println("A divisão entre os valores inseridos é " + (n1 / n2));
-        }
+    public static double div(double n1, double n2){
+        return (n1/n2);
     }
 }
